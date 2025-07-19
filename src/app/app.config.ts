1ca0063d5ce9f4f8aa249a8a7a3dd72fbbea8d78
Kusillo <colectivoenrojo@gmail.com>
2025-07-19 13:39:12 -0400

    feat: integrate Firebase services and update app configuration
    - Added @angular/fire package to package.json for Firebase integration.
    - Created a new index.html file for Firebase Hosting setup with necessary SDK scripts.
    - Updated app.config.ts to initialize Firebase services including Auth, Firestore, Analytics, Database, Messaging, and Storage.
    - Added storage.rules file with basic security rules for Firebase Storage.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,14 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "my-tutors-click", appId: "1:786942767642:web:59559405eae7f2432817a3", storageBucket: "my-tutors-click.firebasestorage.app", apiKey: "AIzaSyC7TXHUA6fk2F6F1jf8OKTQCLl69ucoeDY", authDomain: "my-tutors-click.firebaseapp.com", messagingSenderId: "786942767642", measurementId: "G-QYWRHLS8PG" })), provideAuth(() => getAuth()), provideAnalytics(() => getAnalytics()), ScreenTrackingService, UserTrackingService, provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())]
 };
